5e0df405372ed6089c7ed7fa5a1b03266ba62d44
Jakub Labaj <j.labaj@marmind.com>
2021-02-04 14:01:31 +0100

    Revert "fix storage ts error"
    This reverts commit 2d647d2206d0ab5bd05633eebbdbd0fdd321e6b9.

--- src/core/storage/engines/memory-storage-provider.ts
+++ src/core/storage/engines/memory-storage-provider.ts
@@ -20,7 +20,7 @@ export class MemoryStorageProvider<T = StorageValueType>
 	}
 
 	get<R = T>(key: string): R | void {
-		return this.data.get(key) as unknown as R | void;
+		return this.data.get(key) as R | void;
 	}
 
 	exists(key: string): boolean {
